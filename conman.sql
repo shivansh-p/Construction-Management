158bc3feeba71e5746a8295b41c5ea4c2daee0a4
Vaibhav Bhujade <vaibhavbhujade98@gmail.com>
2019-09-28 21:03:10 +0530

    Database updated

--- conman.sql
+++ conman.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2019 at 06:06 PM
+-- Generation Time: Sep 28, 2019 at 05:32 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -25,6 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `progressreport`
+--
+
+CREATE TABLE `progressreport` (
+  `SubstructureID` int(11) NOT NULL,
+  `Overall_Progress` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `progressreport`
+--
+
+INSERT INTO `progressreport` (`SubstructureID`, `Overall_Progress`) VALUES
+(38, 39.3),
+(48, 15.63);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `projectschedule`
 --
 
@@ -34,6 +53,24 @@ CREATE TABLE `projectschedule` (
   `Start_Date` date NOT NULL,
   `Total_Days` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `projectschedule`
+--
+
+INSERT INTO `projectschedule` (`Name`, `SubstructureID`, `Start_Date`, `Total_Days`) VALUES
+('Test', 37, '2019-09-28', 15),
+('New', 38, '2019-09-30', 10),
+('New', 39, '2019-09-29', 20),
+('New', 40, '2019-09-29', 20),
+('New', 41, '2019-09-29', 20),
+('New', 42, '2019-09-29', 20),
+('New', 43, '2019-09-29', 20),
+('New', 44, '2019-09-29', 20),
+('New', 45, '2019-09-29', 20),
+('New', 46, '2019-09-29', 20),
+('New', 47, '2019-09-29', 20),
+('Demo', 48, '2019-09-30', 10);
 
 -- --------------------------------------------------------
 
@@ -50,8 +87,65 @@ CREATE TABLE `workreport` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `workreport`
+--
+
+INSERT INTO `workreport` (`SubstructureID`, `Date`, `IdealWork`, `ActualWork`, `Progress`) VALUES
+(37, '2019-09-28', 6.66667, 3.5, -47.5),
+(37, '2019-09-29', 6.66667, 0, 0),
+(37, '2019-09-30', 6.66667, 0, 0),
+(37, '2019-10-01', 6.66667, 0, 0),
+(37, '2019-10-02', 6.66667, 0, 0),
+(37, '2019-10-03', 6.66667, 0, 0),
+(37, '2019-10-04', 6.66667, 3.5, -47.5),
+(37, '2019-10-05', 6.66667, 5, -25),
+(37, '2019-10-06', 6.66667, 5, -25),
+(37, '2019-10-07', 6.66667, 0, 0),
+(37, '2019-10-08', 6.66667, 0, 0),
+(37, '2019-10-09', 6.66667, 0, 0),
+(37, '2019-10-10', 6.66667, 0, 0),
+(37, '2019-10-11', 6.66667, 0, 0),
+(37, '2019-10-12', 6.66667, 0, 0),
+(38, '2019-09-29', 5, 0, 0),
+(38, '2019-09-30', 10, 5.8, 16),
+(38, '2019-10-01', 10, 3.5, -30),
+(38, '2019-10-02', 10, 0, 0),
+(38, '2019-10-03', 10, 0, 0),
+(38, '2019-10-04', 10, 0, 0),
+(38, '2019-10-05', 10, 0, 0),
+(38, '2019-10-06', 10, 5, 0),
+(38, '2019-10-07', 10, 0, 0),
+(38, '2019-10-08', 10, 0, 0),
+(38, '2019-10-09', 10, 0, 0),
+(38, '2019-10-10', 5, 0, 0),
+(38, '2019-10-11', 5, 0, 0),
+(38, '2019-10-12', 5, 0, 0),
+(38, '2019-10-13', 5, 0, 0),
+(38, '2019-10-14', 5, 0, 0),
+(38, '2019-10-15', 5, 0, 0),
+(38, '2019-10-16', 5, 0, 0),
+(38, '2019-10-17', 5, 0, 0),
+(38, '2019-10-18', 5, 0, 0),
+(48, '2019-09-30', 10, 5.63, 56.3),
+(48, '2019-10-01', 10, 10, 100),
+(48, '2019-10-02', 10, 0, 0),
+(48, '2019-10-03', 10, 0, 0),
+(48, '2019-10-04', 10, 0, 0),
+(48, '2019-10-05', 10, 0, 0),
+(48, '2019-10-06', 10, 0, 0),
+(48, '2019-10-07', 10, 0, 0),
+(48, '2019-10-08', 10, 0, 0),
+(48, '2019-10-09', 10, 0, 0);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `progressreport`
+--
+ALTER TABLE `progressreport`
+  ADD PRIMARY KEY (`SubstructureID`);
 
 --
 -- Indexes for table `projectschedule`
@@ -73,7 +167,7 @@ ALTER TABLE `workreport`
 -- AUTO_INCREMENT for table `projectschedule`
 --
 ALTER TABLE `projectschedule`
-  MODIFY `SubstructureID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `SubstructureID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
