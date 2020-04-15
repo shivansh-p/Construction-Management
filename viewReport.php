a6e5c5b75545fd04f11b834a4a3338e130dade96
shreyzo <zodeshreya@gmail.com>
2020-04-15 17:08:38 +0530

    Progress Bar color

--- viewReport.php
+++ viewReport.php
@@ -22,11 +22,47 @@ $result2=mysqli_query($conn,$query2);
 $row2=mysqli_fetch_array($result2);
 $pro=$row2['Overall_Progress'];
 
-echo "<div class='c'>";
-echo "Substructure Total Progress - ". $pro ."%";
-echo "</div>";
+
 $query="select * from workreport where SubstructureID='$id' ";
 $result=mysqli_query($conn,$query);
+
+//calculating  progress
+$result5 = mysqli_query($conn,$query);
+$numerator = 0;
+$denominator = 0;
+$progress = 0;
+if ($result5->num_rows > 0)
+{
+    // output data of each row
+    while($row5 = $result5->fetch_assoc())
+    {
+        if($row5["ActualWork"]>0)
+        {
+            $numerator = $numerator + $row5["ActualWork"];
+            $denominator = $denominator + $row5["IdealWork"];
+        }
+    }
+    $progress = ($numerator/$denominator)*100;
+}
+else
+{
+    echo "0 results";
+}
+//Calculating progress done
+echo "<div class='c'>";
+//Progress bar color check
+if($progress < 80)
+{
+    echo "<style> .c {background-color: red;}</style>";
+    //background-color: #33cc00;
+}
+else
+{
+    echo "<style> .c {background-color: #33cc00;}</style>";
+}
+echo "Substructure Total Progress - ". $progress ."%";
+echo "</div>";
+
 
 echo "<table border='1'>
 <tr>
@@ -43,7 +79,7 @@ echo "<table border='1'>
 <button onclick="optimizebtn()"  >Click here to Optimize</button>
 <div id="optimize" style="display:none;">
   <?php
-  
+
   include 'calculations.php';?>
 </div>
 <script>
