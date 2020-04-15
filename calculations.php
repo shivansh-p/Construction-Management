2d3462d370e7a00ae98dec249add14858ada5eab
Vaibhav Bhujade <vaibhavbhujade98@gmail.com>
2020-04-15 17:10:23 +0530

    Earthworth acitivity calculation

--- calculations.php
+++ calculations.php
@@ -9,38 +9,77 @@ include 'DatabaseConnection.php';
 #$row1=mysqli_fetch_array($result1);
 #$id=$row['SubstructureID'];
 //$id=1;
+if($Name=="Earthwork")
+{
+  $query4="select * from workreport where SubstructureID='$id' ";
+  $result4=mysqli_query($conn,$query4);
 
-$query4="select * from workreport where SubstructureID='$id' ";
-$result4=mysqli_query($conn,$query4);
+  $row4 = $result4->fetch_assoc();
+  $days=0;
+  $sumActualWork=0;
+  while($row4["ActualWork"]>0) {
+    $days=$days+1;
+    $sumActualWork=$sumActualWork+$row4["ActualWork"];
+    $row4 = $result4->fetch_assoc();
+  }
 
-$row = $result4->fetch_assoc();
-$days=0;
-$sumActualWork=0;
-while($row["ActualWork"]>0) {
-  $days=$days+1;
-  $sumActualWork=$sumActualWork+$row["ActualWork"];
-  $row = $result4->fetch_assoc();
+  $query3="select * from newprojectschedule where SubstructureID='$id'";
+  $result3=mysqli_query($conn,$query3);
+  $row3=mysqli_fetch_array($result3);
+  $Quantity=$row3["Quantity"];
+  $Duration=$row3["Total_Days"];
+  $RL=$row3["Labour_Rate"];
+  $RM=$row3["Machine_Rate"];
+
+
+  $Q=$Quantity*(1-($days/$Duration));
+  $q=(($Quantity/$Duration)*$days)-$sumActualWork;
+  $Zoptimised=($Q*$RM+$Q*0.01*$RL)+($q*$RM+$q*0.01*(1.5*$RL));
+  $Z=($Q*$RM+$Q*0.01*$RL)+($q*$RM+$q*0.01*$RL);
+  $percentage=($Zoptimised/$Z)*100;
+
+  echo "Work should be done in regular time - ".round($Q,2)."<br>";
+  echo "Work should be done in overtime - ".round($q,2)."<br>";
+  echo "Cost of the optimized solution - ".round($Zoptimised,2)."<br>";
+  echo "Cost of the regular solution - ".round($Z,2)."<br>";
+  echo "Percentage cost of optimized solution w.r.t regular solution - ".round($percentage,2)."<br>";
+
 }
+else {
+  // code...
+  $query4="select * from workreport where SubstructureID='$id' ";
+  $result4=mysqli_query($conn,$query4);
 
-$query3="select * from newprojectschedule where SubstructureID='$id'";
-$result3=mysqli_query($conn,$query3);
-$row3=mysqli_fetch_array($result3);
-$Quantity=$row3["Quantity"];
-$Duration=$row3["Total_Days"];
-$RL=$row3["Labour_Rate"];
-$RM=$row3["Machine_Rate"];
+  $row4 = $result4->fetch_assoc();
+  $days=0;
+  $sumActualWork=0;
+  while($row4["ActualWork"]>0) {
+    $days=$days+1;
+    $sumActualWork=$sumActualWork+$row4["ActualWork"];
+    $row4 = $result4->fetch_assoc();
+  }
+
+  $query3="select * from newprojectschedule where SubstructureID='$id'";
+  $result3=mysqli_query($conn,$query3);
+  $row3=mysqli_fetch_array($result3);
+  $Quantity=$row3["Quantity"];
+  $Duration=$row3["Total_Days"];
+  $RL=$row3["Labour_Rate"];
+  $RM=$row3["Machine_Rate"];
 
 
-$Q=$Quantity*(1-($days/$Duration));
-$q=(($Quantity/$Duration)*$days)-$sumActualWork;
-$Zoptimised=$Q*($RM+$RL)+$q*($RM+(1.5*$RL));
-$Z=($Q+$q)*($RM+$RL);
-$percentage=($Zoptimised/$Z)*100;
+  $Q=$Quantity*(1-($days/$Duration));
+  $q=(($Quantity/$Duration)*$days)-$sumActualWork;
+  $Zoptimised=$Q*($RM+$RL)+$q*($RM+(1.5*$RL));
+  $Z=($Q+$q)*($RM+$RL);
+  $percentage=($Zoptimised/$Z)*100;
 
-echo "Work should be done in regular time - ".$Q."<br>";
-echo "Work should be done in overtime - ".$q."<br>";
-echo "Cost of the optimized solution - ".$Zoptimised."<br>";
-echo "Cost of the regular solution - ".$Z."<br>";
-echo "Percentage cost of optimized solution w.r.t regular solution - ".$percentage."<br>";
+  echo "Work should be done in regular time - ".round($Q,2)."<br>";
+  echo "Work should be done in overtime - ".round($q,2)."<br>";
+  echo "Cost of the optimized solution - ".round($Zoptimised,2)."<br>";
+  echo "Cost of the regular solution - ".round($Z,2)."<br>";
+  echo "Percentage cost of optimized solution w.r.t regular solution - ".round($percentage,2)."<br>";
+
+}
 
 ?>
